1a24bfb387f464224b244ea192eee505cbcecff5
naive-fox <931476127@qq.com>
2022-06-25 18:25:15 +0800

    add insert desktop

--- Notes/Notes.cpp
+++ Notes/Notes.cpp
@@ -4,18 +4,35 @@ Notes::Notes(QMainWindow *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    m_lockstatus = false;
 
+    this->setWindowTitle("MyNotes");
 
     //设置无边框
     //this->setWindowFlags(Qt::FramelessWindowHint);
     //设置背景颜色
-    this->setStyleSheet("background:#67BBF0");
+    //this->setStyleSheet("background:#67BBF0");
     //设置透明化
-    //this->setWindowOpacity(0.6);
+    this->setWindowOpacity(0.6);
 
 
     InitSystemTrayIcon();
 
+    
+}
+
+Notes::~Notes()
+{
+    if (nullptr == m_systemTray)
+        delete m_systemTray;
+    if (nullptr == m_menu)
+        delete m_menu;
+    if (nullptr == m_lockwindow)
+        delete  m_lockwindow;
+    if (nullptr == m_showMainWindowAction)
+        delete m_showMainWindowAction;
+    if (nullptr == m_exitAction)
+        delete m_exitAction;
 }
 
 void Notes::InitSystemTrayIcon()
@@ -58,16 +75,88 @@ void Notes::CreateAction()
     connect(m_showMainWindowAction, SIGNAL(triggered()), this, SLOT(on_showMainAction()));
     m_exitAction = new QAction(QStringLiteral("退出"), this);
     connect(m_exitAction, SIGNAL(triggered()), this, SLOT(on_exitAppAction()));
+    m_lockwindow = new QAction(QStringLiteral("取消锁定"), this);
+    connect(m_lockwindow, SIGNAL(triggered()), this, SLOT(lockwindow()));
 }
 
 void Notes::CreateMenu()
 {
     m_menu = new QMenu(this);
     m_menu->addAction(m_showMainWindowAction);
+    m_menu->addAction(m_lockwindow);
     m_menu->addSeparator();
     m_menu->addAction(m_exitAction);
 
     m_systemTray->setContextMenu(m_menu);
+}
+
+void Notes::InsertDesktop()
+{
+    if (m_lockstatus)
+    {
+        HWND hwndWorkerW = nullptr;
+        HWND hwndDefView = nullptr;
+
+        hwndWorkerW = FindWindowEx(NULL, NULL, L"WorkerW", NULL);
+        while ((!hwndDefView) && hwndWorkerW)
+        {
+            hwndDefView = FindWindowEx(NULL, NULL, L"SHELLDLL_DefView", NULL);
+            hwndWorkerW = FindWindowEx(NULL, hwndWorkerW, L"WorkerW", NULL);
+        }
+
+        SetParent((HWND)this->winId(), (HWND)0x0001019C);
+
+        ShowWindow(hwndWorkerW, 0);
+    }
+    else
+    {
+        SetParent((HWND)this->winId(), NULL);
+    }
+    m_lockstatus = !m_lockstatus;
+/*
+
+    bool enumUserWindowsCB(HWND hwnd, LPARAM lParam)
+    {
+        long wflags = GetWindowLong(hwnd, GWL_STYLE);
+        if (!(wflags & WS_VISIBLE)) {
+            return TRUE;
+        };
+
+        HWND sndWnd;
+        if (!(sndWnd = FindWindowEx(hwnd, NULL, L"SHELLDLL_DefView", NULL))) {
+            return TRUE;
+        }
+        HWND targetWnd;
+        if (!(targetWnd = FindWindowEx(sndWnd, NULL, L"SysListView32", L"FolderView"))) {
+            return TRUE;
+        }
+
+        HWND* resultHwnd = (HWND*)lParam;
+        *resultHwnd = targetWnd;
+        return FALSE;
+    }
+
+
+    MainWindow::MainWindow(QWidget * parent)
+        : QMainWindow(parent)
+        , ui(new Ui::MainWindow)
+    {
+        ui->setupUi(this);
+
+
+        HWND resultHwnd = NULL;
+        EnumWindows((WNDENUMPROC)enumUserWindowsCB, (LPARAM)&resultHwnd);
+        HWND desktopHwnd = resultHwnd; //findDesktopIconWnd();
+        if (desktopHwnd)
+            SetParent((HWND)this->winId(), desktopHwnd);
+
+    }
+   */
+}
+
+void Notes::lockwindow()
+{
+    InsertDesktop();
 }
 
 void Notes::on_showMainAction()
@@ -78,6 +167,7 @@ void Notes::on_showMainAction()
 
 void Notes::on_exitAppAction()
 {
+    //delete m_systemTray;
     qApp->exit();
 }
 
--- Notes/Notes.h
+++ Notes/Notes.h
@@ -7,6 +7,7 @@
 #include <QCloseEvent>
 #include <QMessageBox>
 #include "ui_Notes.h"
+#include <Windows.h>
 
 class Notes : public QMainWindow
 {
@@ -14,24 +15,32 @@ class Notes : public QMainWindow
 
 public:
     Notes(QMainWindow *parent = Q_NULLPTR);
-
+    ~Notes();
 
 private:
     void InitSystemTrayIcon();
     void CreateAction();
     void CreateMenu();
+    void InsertDesktop();
 private slots:
+    void lockwindow();
     void on_showMainAction();
     void on_exitAppAction();
 
 protected:
     void closeEvent(QCloseEvent* event) override;
 private:
+    //Õ–≈ÃÕº±Í
     QSystemTrayIcon* m_systemTray;
+    //Õ–≈Ã”“º¸≤Àµ•
     QMenu* m_menu;
+    //Õ–≈Ã”“º¸≤Àµ•∂‘”¶µƒAction
+    QAction* m_lockwindow;
     QAction* m_showMainWindowAction;
     QAction* m_exitAction;
 
+    bool m_lockstatus;
+    HWND m_hwndMywindow;
 private:
     Ui::NotesClass ui;
 };
